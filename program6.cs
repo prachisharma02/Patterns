7c4eade0952e8b68939ef014c28a57480736b052
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-03-04 18:38:23 +0530

    completed 4th

--- program6.cs
+++ program6.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Pattern
 {
-    internal class Program4
+    internal class Program6
     {
         public void Ptt()
         {
-            Console.WriteLine("4.  Number-increasing reverse Pyramid Pattern:");
+            Console.WriteLine("6. Zero-One Triangle Pattern:");
             Console.Write("enter the no of rows : ");
             int n = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i <= n; i++)
             {
-                for (int j = 1; j <=n-i; j++)
+                for (int j = 1; j <= i; j++)
                 {
                     Console.Write(j);
                 }
